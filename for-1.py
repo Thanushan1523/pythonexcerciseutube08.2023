0ec9386857d28f98652cc7206ea94e92902f5852
Thanushan1523 <thanushan1523@gmail.com>
2023-08-18 19:14:06 -0400

    for loop odd & even sum with range function

--- for-1.py
+++ for-1.py
@@ -16,8 +16,79 @@
 # for i in range(1,10):
 #     print(i,"x2 =" ,i*2)
 
-a=int(input("a:"))
-b=int(input("b:"))
-for i in range(a,b):
-    if (i%2==0):
-     print(i)
+# a=int(input("a:"))
+# b=int(input("b:"))
+# for i in range(a,b):
+#     if (i%2==0):
+#      print(i)
+        # FIND THE MISTAKE
+# e_count=0
+# o_count=0
+# a=int(input("a:"))
+# b=int(input("b:"))
+# for i in range(a,b):
+#     if (i%2==0):
+#       e_count=e_count +1
+#     print("even",e_count)
+# else:
+#     o_count=o_count+1
+#     print("odd",o_count)
+
+        # FIND THE MISTAKE
+# e_count=0
+# o_count=0
+# a=int(input("a:"))
+# b=int(input("b:"))
+# for i in range(a,b):
+#     if (i%2==0):
+#       e_count=e_count +1
+    
+# else:
+#         o_count=o_count+1
+# print("even",e_count)
+# print("odd",o_count)
+
+# e_count=0
+# o_count=0
+# a=int(input("a:"))
+# b=int(input("b:"))
+# for i in range(a,b):
+#     if (i%2==0):
+#       e_count=e_count +1
+    
+#     else:
+#         o_count=o_count+1
+# print("even",e_count)
+# print("odd",o_count)
+
+
+# count=0
+# a=int(input("a:"))
+# b=int(input("b:"))
+# for i in range(a,b):
+#     if (i%3==0 and i%5==0):
+#         count=count+1
+# print(count)
+
+# sum=0
+# for i in range(1,15):
+#     sum=sum+ i
+# print(sum)
+
+
+# a=int(input("a:"))
+# b=int(input("b:"))
+# sum=0
+# for i in range(a,b):
+#     sum=sum+ i
+# print("sum of",a,"to",b , "is",sum)
+
+
+
+
+a=[]
+for i in range(1,4):
+        
+        num=int(input("enter number" +i)) 
+        a.append(num)
+print(a)             
