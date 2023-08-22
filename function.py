1b2086e07658f0ecc1e077dd528d21e468218297
Thanushan1523 <thanushan1523@gmail.com>
2023-08-22 19:23:57 -0400

    self & constructor

--- function.py
+++ function.py
@@ -6,14 +6,31 @@
 # add()
 
 
-class laptop:
-    price=input("enter ram price:")
-    processor=""
-    ram=""
+# class laptop:
+#     price=input("enter ram price:")
+#     processor=""
+#     ram=""
 
-hp= laptop()
-# hp.price=5000
-# hp.processor="i5"
-# hp.ram=input("enter ram capacity:")
+# hp= laptop()
+# # hp.price=5000
+# # hp.processor="i5"
+# # hp.ram=input("enter ram capacity:")
 
-print("price is ",hp.price)
+# print("price is ",hp.price)
+
+
+
+class laptop():
+    def __init__(self):
+        self.ram=""
+        self.processor=""
+    def display (self):
+        print("ram",self.ram)
+        print("processor",self.processor)
+
+hp=laptop()
+
+hp.processor="i7"
+hp.ram="i7"
+
+hp.display()
