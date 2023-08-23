8bb476972d86d227f6b3918336e8dff4d6525bd1
Thanushan1523 <thanushan1523@gmail.com>
2023-08-23 08:04:05 -0400

    types of class method2

--- typeofclassvariable.py
+++ typeofclassvariable.py
@@ -1,3 +1,6 @@
+# ##instance mtd,cls mtd, ststic mtd 
+
+
 # class laptop:
 #     chargertype="C type"
 
@@ -15,6 +18,47 @@
 # hp.getprice()
 
 
+# class laptop:
+#     chargertype="C type"
+
+#     def __init__(self):
+#         self.price=34
+#         self.brand=""
+
+#     def setprice(self,price):
+#         self.price=price
+
+#     def getprice(self):
+#         print(self.price)
+
+# hp=laptop()
+# hp.setprice(15000)
+# hp.getprice()
+
+# class laptop:
+#     chargertype="C type"
+
+#     def __init__(self):
+#         self.price=34
+#         self.brand=""
+
+#     def setprice(self,price):
+#         self.price=price
+
+#     def getprice(self):
+#         print(self.price)
+    
+#     @classmethod
+#     def changechargertype(cls):
+#         cls.chargertype="b type"
+#         print("chargertype changed to B")
+# hp=laptop()
+# hp.setprice(15000)
+# hp.getprice()
+# laptop.changechargertype(laptop)
+
+
+
 class laptop:
     chargertype="C type"
 
@@ -27,7 +71,19 @@ class laptop:
 
     def getprice(self):
         print(self.price)
+    
+    @classmethod
+    def changechargertype(cls):
+        cls.chargertype="b type"
+        print("chargertype changed to B")
+
+    @staticmethod
+    def info ():
+        print("this is laptop class")
+
 
 hp=laptop()
 hp.setprice(15000)
 hp.getprice()
+hp.changechargertype(laptop)
+hp.info()
