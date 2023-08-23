f53baa385ade103fe3bb940feb4dea99e447529d
Thanushan1523 <thanushan1523@gmail.com>
2023-08-23 08:31:51 -0400

    multiple level inheritance

--- inheritance.py
+++ inheritance.py
@@ -1,15 +1,33 @@
-class dad():
-    def phone(self):
-        print("dad's phone")
+# class dad():
+#     def phone(self):
+#         print("dad's phone")
 
-class mom():
-    def sweet (self):
-        print("mom's sweet")
+# class mom():
+#     def sweet (self):
+#         print("mom's sweet")
 
-class son(dad,mom):
+# class son(dad,mom):
+#     def laptop(self):
+#         print("son's laptop")
+
+# ram=son()
+# ram.phone()
+# ram.sweet()
+
+
+class grandfather():
+    def house(self):
+        print("grandfather's home")
+
+class appa(grandfather):
+    def car(self):
+        print("dad's car")
+
+class me (appa):
     def laptop(self):
-        print("son's laptop")
+        print("my laptop")
 
-ram=son()
-ram.phone()
-ram.sweet()
+thanu=me()
+thanu.laptop()
+thanu.car()
+thanu.house()        
