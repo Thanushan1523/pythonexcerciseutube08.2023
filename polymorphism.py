55a2b6fcc1b4414d1e3d76bcb8b22ebe9fa58c75
Thanushan1523 <thanushan1523@gmail.com>
2023-09-18 07:41:20 -0400

    polymorphsom

--- polymorphism.py
+++ polymorphism.py
@@ -1,7 +1,23 @@
-class Animal():
-    def sound(self):
-        print("animal makes sound")
+# class Animal():
+#     def sound(self):
+#         print("animal makes sound")
 
-a1=Animal()
-a1.sound()
-print(a1.sound)
+# a1=Animal()
+# a1.sound()
+# print(a1.sound)
+
+# class Dog(Animal):
+#    def sound(self):
+#     print("dog barkes") 
+
+# a1=Animal()
+# a1.sound()
+# print(a1.sound)
+# d1=Dog()
+# d1.sound()
+# print(d1.sound)
+
+class Shape ():
+    def area ():
+        return 0
+    
