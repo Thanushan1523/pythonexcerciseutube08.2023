dab54a1bb1317b098a0f6433a5808a0cf4ba9cda
Thanushan1523 <thanushan1523@gmail.com>
2023-09-21 07:37:09 -0400

    polymorphsom

--- polymorphism.py
+++ polymorphism.py
@@ -31,10 +31,42 @@
 # r1.area()
 # print(r1.area())
 
-class Person():
-    def __init__(self,name):
-        self.name=name
+# class Person():
+#     def __init__(self,name):
+#         self.name=name
 
-class Student (Person):
-    def __init__(self,grade):
-        self.grade=grade
+# class Student (Person):
+#     def __init__(self,name,grade):
+#         super().__init__(name)
+#         self.grade=grade
+
+#     def display(self):
+#         print(self.name , self.grade)
+
+# s1=Student("hi","A")
+# print(s1.display())
+
+# DOUBT
+# class vehile():
+#     def start():
+#         print("vehile started")
+
+# class car(vehile):
+#     super() .__init__(start)
+
+class employee():
+    def __init__(self,name,salary):
+        self.name=name
+        self.salary=salary
+
+class manager(employee):
+    def __init__(self,name,salary, department):
+       super().__init__(name,salary)
+       
+       self.department=department
+        
+    def display(self):
+        print(self.name,self.salary,self.department)
+
+M1= manager("KAJAN","52","ACC")
+M1.display()
