a75fa5678a235f895ca72a95fbc0a7caadc7fe88
Thanushan1523 <thanushan1523@gmail.com>
2023-09-22 07:52:11 -0400

    encapsulation

--- encapsulation.py
+++ encapsulation.py
@@ -1,1 +1,14 @@
+# private variable
+
+class company ():
+    def __init__(self):
+        self.__companyname="Genious"
+
+    def companyname(self):
+        print(self.__companyname)
+
+c1=company()
+c1.companyname
+
+
 print("hi")
